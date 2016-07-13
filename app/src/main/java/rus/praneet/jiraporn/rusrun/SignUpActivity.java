b93587843e36f3784e74b59bdb1f9b3a51691431
jiraporn <Nut.jirapornpraneet@gmail.com>
2016-07-13 11:12:29 +0700

    CheckChoose

--- app/src/main/java/rus/praneet/jiraporn/rusrun/SignUpActivity.java
+++ app/src/main/java/rus/praneet/jiraporn/rusrun/SignUpActivity.java
@@ -34,6 +34,33 @@ public class SignUpActivity extends AppCompatActivity {
         avata3RadioButton = (RadioButton) findViewById(R.id.radioButton4);
         avata4RadioButton = (RadioButton) findViewById(R.id.radioButton5);
 
+        //Radio Controller
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                switch (i) {
+
+                    case R.id.radioButton:
+                        avataString = "0";
+                        break;
+                    case R.id.radioButton2:
+                        avataString = "1";
+                        break;
+                    case R.id.radioButton3:
+                        avataString = "2";
+                        break;
+                    case R.id.radioButton4:
+                        avataString = "3";
+                        break;
+                    case R.id.radioButton5:
+                        avataString = "4";
+                        break;
+                }   //switch
+
+            }
+        });
+
     }//Main Method
 
     public void clickSignUpSign (View view){
@@ -49,11 +76,33 @@ public class SignUpActivity extends AppCompatActivity {
             MyAlert myAlert = new MyAlert();
             myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่องค่ะ");
 
+        } else if (checkChoose()) {
+            //UnChecked
+
+        } else {
+            //Un Check
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "ยังไม่เลือก Avata",
+                    "กรุณาเลือก Avata ด้วยค่ะ");
+
 
         }
 
 
 
     } //clickSignUp
+
+    private boolean checkChoose() {
+
+        boolean status = true;
+
+        status = avata0RadioButton.isChecked() ||
+                avata1RadioButton.isChecked() ||
+                avata2RadioButton.isChecked() ||
+                avata3RadioButton.isChecked() ||
+                avata4RadioButton.isChecked();
+
+        return status;
+    }
 
 } //Main Class
