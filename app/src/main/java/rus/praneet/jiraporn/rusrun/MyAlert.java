b060d0f4a911aaaffa131a02c497b9ac97981fbc
jiraporn <Nut.jirapornpraneet@gmail.com>
2016-07-13 09:47:29 +0700

    MyAlert

---
+++ app/src/main/java/rus/praneet/jiraporn/rusrun/MyAlert.java
@@ -0,0 +1,17 @@
+package rus.praneet.jiraporn.rusrun;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+/**
+ * Created by Administrator on 13/7/2559.
+ */
+public class MyAlert {
+
+    public void myDialog(Context context,
+                         String strTitle,
+                         String strMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+    }
+
+} // Main Class
